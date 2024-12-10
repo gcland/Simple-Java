7623a064efcd393744513be8535725bd61d5205a
gcland <grantbcopeland@gmail.com>
2024-12-10 09:21:33 -0600

    Initial java package and exploration of java
    Created initial hello world files for java. Will continue to understand java code / file structure

---
+++ JavaProject/src/JavaProject/Main.java
@@ -0,0 +1,10 @@
+package JavaProject;
+
+public class Main { // class
+
+    public static void main(String[] args) { // method
+        // TODO 
+        System.out.println("Hello World!");
+        System.out.println("Hello World!2");
+    }
+}
